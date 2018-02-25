d406bc4d302fa7ff7be6ec7a03e2ad95b2d572a4
vincekd <vincekd@gmail.com>
2018-02-24 22:08:08 -0500

    service worker eventually fresh for tweets/user objects

--- src/ts/service-worker.ts
+++ src/ts/service-worker.ts
@@ -1,11 +1,11 @@
 
-const VERSION = "v0.0.3";
+const VERSION = "v0.0.5";
 const RESOURCE_CACHE = "tapp.resources." + VERSION;
 const USER_CACHE = "tapp.user." + VERSION;
 const TWEET_CACHE = "tapp.tweets." + VERSION;
 const CACHE_WHITELIST = [RESOURCE_CACHE, USER_CACHE, TWEET_CACHE];
 const USER_URL = "/user";
-const TWEET_URLS = ["/tweets/best", "/tweets/latest", "/tweets/search"];
+const TWEET_REGEXP = /\/tweets\/(?:best|latest)(?:\\?.*){0,1}$/i;
 
 const CACHED_RESOURCES = [
     // CSS
@@ -39,51 +39,31 @@ self.addEventListener("error", function(err) {
 });
 
 self.addEventListener("fetch", function(event: any) {
-    //event.waitUntil(new Promise(resolve => {
     const isResource = CACHED_RESOURCES.some(r => event.request.url.endsWith(r));
     const isUser = event.request.url.endsWith(USER_URL);
-    const isTweets = TWEET_URLS.some(url => event.request.url.endsWith(url));
+    const isTweets = TWEET_REGEXP.test(event.request.url);
     if (isResource || isUser || isTweets) {
-        event.respondWith(caches.match(event.request, {ignoreSearch: true}).then(resp => {
-            return resp || fetch(event.request).then(resp => {
-                if (isUser || isResource) {
-                    return caches.open(isUser ? USER_CACHE : RESOURCE_CACHE).then(cache => {
+        event.respondWith(caches.match(event.request).then(resp => {
+            if (isResource) {
+                return resp || fetch(event.request).then(resp => {
+                    return caches.open(RESOURCE_CACHE).then(cache => {
                         cache.put(event.request, resp.clone());
                         return resp;
                     });
-                } else {
-                    console.log("is tweets");
-                    return resp;
-                }
-            });
+                });
+            } else {
+                // eventually fresh
+                let req = fetch(event.request).then(resp => {
+                    return caches.open(isUser ? USER_CACHE : TWEET_CACHE).then(cache => {
+                        cache.put(event.request, resp.clone());
+                        return resp;
+                    });
+                });
+
+                return resp || req;
+            }
         }));
-    } else {
-        console.log("skipping cache", event.request.url);
     }
-    //resolve();
-    //}));
-    // if (CACHED_RESOURCES.some(r => event.request.url.endsWith(r))) {
-    //     event.respondWith(caches.match(event.request).then(resp => {
-    //         return resp || fetch(event.request).then(resp => {
-    //             return caches.open(RESOURCE_CACHE).then(cache => {
-    //                 cache.put(event.request, resp.clone());
-    //                 return resp;
-    //             });
-    //         });
-    //     }));
-    // } else if (event.request.url.endsWith(USER_URL)) {
-    //     console.log("fetch/store url", event.request.url);
-    //     event.respondWith(caches.match(event.request)).then(resp => {
-    //         return resp || fetch(event.request).then(resp => {
-    //             caches.open(USER_CACHE).then(cache => {
-    //                 cache.put(event.request, resp.clone());
-    //                 return resp;
-    //             });
-    //         });
-    //     });
-    // } else {
-    //     console.log("skipping cache", event.request.url);
-    // }
 });
 
 self.addEventListener("activate", function(event: any) {
@@ -105,9 +85,8 @@ self.addEventListener("install", function(event: any) {
     console.log("installing service worker");
     event.waitUntil(
         Promise.all([
-            caches.open(RESOURCE_CACHE).then(cache => cache.addAll(CACHED_RESOURCES)),
+            caches.open(RESOURCE_CACHE).then(cache => cache.addAll(CACHED_RESOURCES.filter(r => r.startsWith("/")))),
             caches.open(USER_CACHE).then(cache => cache.addAll([USER_URL])),
-            //TODO: tweets?
         ])
     );
 });
