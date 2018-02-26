031d52dd136c6abbc74565047a29c4484885efcf
vincekd <vincekd@gmail.com>
2018-02-26 18:21:53 -0500

    loading spinner fix

--- src/ts/app.components.ts
+++ src/ts/app.components.ts
@@ -14,7 +14,6 @@ import {
 import { User, Tweet } from './app.classes';
 import { UserService, TweetService, AnalyticsService } from './app.services';
 import {
-    //fadeInAnimation
     routerTransition
 } from './app.animations'
 
@@ -87,8 +86,6 @@ class TweetsComponent implements OnInit {
 }
 
 @Component({
-    //animations: [fadeInAnimation],
-    //host: {'[@fadeInAnimation]': ''},
     templateUrl: "/templates/tweets.html"
 })
 export class LatestComponent extends TweetsComponent {
@@ -99,8 +96,6 @@ export class LatestComponent extends TweetsComponent {
 }
 
 @Component({
-    //animations: [fadeInAnimation],
-    //host: {'[@fadeInAnimation]': ''},
     templateUrl: "/templates/tweets.html"
 })
 export class BestComponent extends TweetsComponent {
@@ -111,8 +106,6 @@ export class BestComponent extends TweetsComponent {
 }
 
 @Component({
-    //animations: [fadeInAnimation],
-    //host: {'[@fadeInAnimation]': ''},
     templateUrl: "/templates/search.html"
 })
 export class SearchComponent extends TweetsComponent {
@@ -221,8 +214,6 @@ export class SearchComponent extends TweetsComponent {
 }
 
 @Component({
-    //animations: [fadeInAnimation],
-    //host: {'[@fadeInAnimation]': ''},
     template: `
 <div id="single-tweet">
   <div></div>
@@ -274,7 +265,21 @@ export class TweetFragComponent {
 @Component({
     selector: "[loading-spinner]",
     template: `<div>
-<div class="loading-spinner"><i class="icon-circle-notch"></i></div>
+  <div class="loading-spinner">
+    <svg version="1.1" id="loader-1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+         width="40px" height="40px" viewBox="0 0 40 40" enable-background="new 0 0 40 40" xml:space="preserve">
+      <path opacity="0.2" fill="#000" d="M20.201,5.169c-8.254,0-14.946,6.692-14.946,14.946c0,8.255,6.692,14.946,14.946,14.946 s14.946-6.691,14.946-14.946C35.146,11.861,28.455,5.169,20.201,5.169z M20.201,31.749c-6.425,0-11.634-5.208-11.634-11.634 c0-6.425,5.209-11.634,11.634-11.634c6.425,0,11.633,5.209,11.633,11.634C31.834,26.541,26.626,31.749,20.201,31.749z"/>
+      <path fill="#000" d="M26.013,10.047l1.654-2.866c-2.198-1.272-4.743-2.012-7.466-2.012h0v3.312h0 C22.32,8.481,24.301,9.057,26.013,10.047z">
+        <animateTransform attributeType="xml"
+            attributeName="transform"
+            type="rotate"
+            from="0 20 20"
+            to="360 20 20"
+            dur="0.7s"
+            repeatCount="indefinite"/>
+      </path>
+    </svg>
+  </div>
 </div>`
 })
 export class LoadingSpinnerComponent {
@@ -282,8 +287,6 @@ export class LoadingSpinnerComponent {
 }
 
 @Component({
-    //animations: [fadeInAnimation],
-    //host: {'[@fadeInAnimation]': ''},
     templateUrl: "/templates/error.html"
 })
 export class ErrorPageComponent {
