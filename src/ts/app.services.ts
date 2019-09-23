42475731318f3f73442cb00ed5298d1a6478668f
vincekd <vincekd@gmail.com>
2019-09-23 09:06:14 -0500

    fix ga test

--- src/ts/app.services.ts
+++ src/ts/app.services.ts
@@ -80,14 +80,17 @@ export class TweetService {
 
 @Injectable()
 export class AnalyticsService {
+    private enabled: boolean = false;
+
     constructor() {
-        if (typeof "ga" !== "function") {
+      this.enabled = (typeof ga === "function");
+      if (!this.enabled) {
             console.warn("ganalytics not loaded.");
         }
     }
 
     public trackEvent(eventCategory: string, event: string, button: string): void {
-        if (typeof ga === "function") {
+        if (this.enabled) {
             try {
                 ga('send', 'event', eventCategory, event, button);
             } catch (e) {
@@ -97,7 +100,7 @@ export class AnalyticsService {
     }
 
     public trackPage(page: string): void {
-        if (typeof ga === "function") {
+        if (this.enabled) {
             try {
                 ga('send', 'pageview', page);
             } catch (e) {
